819ff8f6b40299ade32f24566958414f0c7776d2
Gothwad Technologies <pawangothwad@gmail.com>
2026-06-13 16:05:13 +0530

    sync: deploy custom code via GrixChat - update src/features/chat/index.ts

---
+++ src/features/chat/index.ts
@@ -0,0 +1,8 @@
+export { default as ChatsTab } from './ChatsTab';
+export { default as ChatScreen } from './ChatScreen';
+export { default as MessagesListScreen } from './MessagesListScreen';
+export { default as ChatLayout } from './ChatLayout';
+export { default as HideChatScreen } from './HideChatScreen';
+export { default as ArchivedChatScreen } from './ArchivedChatScreen';
+export { default as HideChatSettings } from './HideChatSettings';
+export { default as MessageRequestsScreen } from './MessageRequestsScreen';
